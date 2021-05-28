a93320b9687645e9b5f3567c469ace7e3c779f75
Lauriane <lauriane.haydari@gmail.com>
2021-05-28 10:43:33 +0200

    Add initial project

---
+++ RickAndMortyProject/RickAndMortyProject/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  RickAndMortyProject
+//
+//  Created by Lau on 27/05/2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let winScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: winScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+
+}
+
